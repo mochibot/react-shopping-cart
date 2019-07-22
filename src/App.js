b1117ae6567e6e4241e17471c2b4c307b752bca7
mochibot <amusaur@gmail.com>
2019-07-22 12:59:19 -0700

    Persist cart items using localStorage. Prevent item from being added to shopping cart if it is already present in cart.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,34 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+// Hooks;
+import { useLocalStorage } from './hooks/useLocalStorage';
+
+// Context;
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
-import { ProductContext } from './contexts/ProductContext';
-import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const [cart, setCart] = useLocalStorage('cart', []);
 
 	const addItem = item => {
-		setCart([...cart, item]);
+		if (cart.some(element => element.id === item.id)) {
+			alert('Item is already in shopping cart.')
+		} else {
+			setCart([...cart, item]);
+		};
 	};
 
 	const deleteItem = id => {
-		let arr = cart.filter(element => element.id !== id)
-		setCart(arr);
+			let arr = cart.filter(element => element.id !== id)
+			setCart(arr);
 	}
 
 	return (
